c1805aea84eaa1f1245517e55083804654f362f3
leonard <mortuza.7@gmail.com>
2019-09-30 19:46:12 +0600

    On BackPressed On Fragment Design

--- app/src/main/java/com/leonard/unichat/Logfiles/LandingPage.java
+++ app/src/main/java/com/leonard/unichat/Logfiles/LandingPage.java
@@ -1,6 +1,7 @@
 package com.leonard.unichat.Logfiles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
@@ -27,4 +28,39 @@ public class LandingPage extends AppCompatActivity {
 
 
     }
+
+    //  Fragment Replacer
+    protected void replaceFragment () {
+
+        fragmentManager.beginTransaction().replace(R.id.frameContainer, new LandingTwo(),
+                Utils.LandingTwo).commit();
+    }
+
+    // BackPressed Called
+    @Override
+    public void onBackPressed () {
+        Fragment StudentAdminLogin = fragmentManager.findFragmentByTag(Utils.StudentAdminSignin);
+        Fragment StudentAdminSignup = fragmentManager.findFragmentByTag(Utils.StudentAdminSignup);
+        Fragment StudentLogin = fragmentManager.findFragmentByTag(Utils.StudentSignin);
+        Fragment StudentSignup = fragmentManager.findFragmentByTag(Utils.StudentSignup);
+        Fragment TeacherLogin = fragmentManager.findFragmentByTag(Utils.TeacherSignin);
+        Fragment TeacherSignup = fragmentManager.findFragmentByTag(Utils.TeacherSignup);
+
+        if (StudentAdminLogin != null)
+            replaceFragment();
+        else if (StudentAdminSignup != null)
+            replaceFragment();
+        else if (StudentLogin != null)
+            replaceFragment();
+        else if (StudentSignup != null)
+            replaceFragment();
+        else if (TeacherLogin != null)
+            replaceFragment();
+        else if (TeacherSignup != null)
+            replaceFragment();
+        else
+            super.onBackPressed();
+    }
+
+
 }
--- app/src/main/java/com/leonard/unichat/Logfiles/TeacherSignup.java
+++ app/src/main/java/com/leonard/unichat/Logfiles/TeacherSignup.java
@@ -28,4 +28,6 @@ public class TeacherSignup extends Fragment {
     }
 
 
+
+
 }
